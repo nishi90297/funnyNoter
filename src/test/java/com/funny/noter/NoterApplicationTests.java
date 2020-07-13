08d08d4dd22a24a9160cb47fa0b13613a32c2c3c
nishi90297 <nishi.90297@gmail.com>
2020-07-13 20:19:01 +0530

    spring boot project initialization

---
+++ src/test/java/com/funny/noter/NoterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.funny.noter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
